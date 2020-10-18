efa53f874c4f41a558e51d4424ac109d7a0d23de
0x144 <jgiraffe@naver.com>
2020-10-19 00:37:48 +0900

    Add memo
    App.js에 학습한 내용을 적는 메모 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React from "react";
 
-function App() {
-  return (
-    <div>
-      hello
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+  add = () => {
+    this.setState(current => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState(current => ({ count: current.count - 1 }));
+  };
+  componentDidMount() {
+    console.log("component did mount");
+  }
+  render() {
+    return (
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>ADD</button>
+        <button onClick={this.minus}>MINUS</button>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+/*
+MEMO
+1. setState시 현재의 state에 접근하기 적절한 방법 this.setState(current => ({ count: current.count + 1 }));
+2. 매순간 setState를 호출할 때 마다 react는 새로운 state와 함께 render을 호출
+3. Mounting : component가 태어나는 것
+ - constructor()
+ - render()
+ - componentDidMount()
+4. Updating : 의미 그대로
+5. Unmounting : component가 죽는 것 
+*/
