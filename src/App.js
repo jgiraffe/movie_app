902e0e1d3c77669f087672d997d7c36c87cc3496
0x144 <jgiraffe@naver.com>
2020-10-19 01:22:48 +0900

    state part completed
    state 파트까지 완료

--- src/App.js
+++ src/App.js
@@ -2,23 +2,19 @@ import React from "react";
 
 class App extends React.Component {
   state = {
-    count: 0
-  };
-  add = () => {
-    this.setState(current => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }));
+    isLoading: true,
+    movies: []
   };
   componentDidMount() {
-    console.log("component did mount");
+    setTimeout(() => {
+      this.setState({ isLoading: false })
+    }, 2500);
   }
   render() {
+    const { isLoading } = this.state; // es6 magic ~~
     return (
       <div>
-        <h1>The number is {this.state.count}</h1>
-        <button onClick={this.add}>ADD</button>
-        <button onClick={this.minus}>MINUS</button>
+        { isLoading ? "Loaing..." : "We are ready" }
       </div>
     );
   }
@@ -28,12 +24,19 @@ export default App;
 
 /*
 MEMO
-1. setState시 현재의 state에 접근하기 적절한 방법 this.setState(current => ({ count: current.count + 1 }));
-2. 매순간 setState를 호출할 때 마다 react는 새로운 state와 함께 render을 호출
-3. Mounting : component가 태어나는 것
- - constructor()
- - render()
- - componentDidMount()
-4. Updating : 의미 그대로
-5. Unmounting : component가 죽는 것 
+1. component는 function과 class가 있고 <Food fav = "kimchi" asd = {true} /> 같이 호출 : JSX
+ - JSX 문법 안에서 js를 쓰기위해 {}를 사용
+ - fav, asd는 props
+ - function component는 props를 props로 받거나 {fav}로 받을 수 있다.
+ - map 사용 예시
+   <div> { foodILike.map(dish => (<Food key={dish.id} />))} </div>
+2. setState시 현재의 state에 접근하기 적절한 방법 this.setState(current => ({ count: current.count + 1 }));
+3. 매순간 setState를 호출할 때 마다 react는 새로운 state와 함께 render을 호출
+4. Mounting : component가 태어나는 것
+ - constructor() -> render() -> componentDidMount()
+5. Updating : state를 변경할 때
+ - render -> componentDidUpdate()
+6. Unmounting : component가 죽는 것
+ - componentWillUnmount()
+7. const { isLoading } = this.state; -> es6 문법으로 { }안은 객체 
 */
