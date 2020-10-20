5ea871d24f96482565c1bae9de1c0013183575d8
Jaegyun Jung <jgiraffe@naver.com>
2020-10-20 17:33:34 +0900

    Updates

---
+++ precache-manifest.13aff8aaa467bc6db1a40767e0b01ac1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c4d79d1f67fec3508f9dcdf82ec8d0d",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "21c98db84d79677e89db",
+    "url": "/movie_app/static/css/main.4a9e9c25.chunk.css"
+  },
+  {
+    "revision": "5dd82ef1c354ed1cbe77",
+    "url": "/movie_app/static/js/2.11a22306.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/movie_app/static/js/2.11a22306.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21c98db84d79677e89db",
+    "url": "/movie_app/static/js/main.02500a97.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
